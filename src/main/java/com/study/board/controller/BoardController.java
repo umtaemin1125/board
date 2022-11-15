6526ce5c09eef2efd875f94cf50d769945419a77
Taemin Eom <umtaemin1125@naver.com>
2022-11-16 01:50:17 +0900

    paging

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -3,6 +3,10 @@ package com.study.board.controller;
 import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,10 +36,19 @@ public class BoardController {
         return "message";
      }
 
-     @GetMapping("/board/list")
-    public String boardList(Model model){
+     @GetMapping("/board/list")  // BoardList 페이징 처리 (JPA), 페이징 블럭
+    public String boardList(Model model,@PageableDefault(page = 0, size = 10, sort ="id", direction = Sort.Direction.DESC) Pageable pageable){
 
-        model.addAttribute("list",boardService.boardList());
+         Page<Board> list= boardService.boardList(pageable);
+
+         int nowPage = list.getPageable().getPageNumber() +1; // 페이지가 0부터 시작하기때문
+         int startPage = Math.max(nowPage -4, 1) ;
+         int endPage = Math.min(nowPage +5,list.getTotalPages()) ;
+
+        model.addAttribute("list",list);
+        model.addAttribute("nowPage", nowPage); //model에 담아 넘겨줌
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
         return "boardlist";
      }
